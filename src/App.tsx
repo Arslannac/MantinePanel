019ff4ddec1438d135a30e39a911dd59a88803fe
Can Arslan <can.arslan@ekinokssoftware.com>
2023-01-13 13:48:30 +0300

    feat: husky commitlint eslint prettier kurulumları yapıldı

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { Text, Button, Stack } from "@mantine/core";
-import { ThemeProvider } from "./ThemeProvider";
+import React from 'react';
+import { Text, Button, Stack } from '@mantine/core';
+import { ThemeProvider } from './ThemeProvider';
 
 export default function App() {
   return (
     <ThemeProvider>
-      <Stack align="center" mt={50}>
-        <Text size="xl" weight={500}>
+      <Stack align='center' mt={50}>
+        <Text size='xl' weight={500}>
           Welcome to Mantine!
         </Text>
         <Button>Click the button</Button>
--- src/ThemeProvider.tsx
+++ src/ThemeProvider.tsx
@@ -1,7 +1,8 @@
-import { MantineProvider, MantineThemeOverride } from "@mantine/core";
+import React from 'react';
+import { MantineProvider, MantineThemeOverride } from '@mantine/core';
 
 export const theme: MantineThemeOverride = {
-  colorScheme: "dark",
+  colorScheme: 'dark',
 };
 
 interface ThemeProviderProps {
